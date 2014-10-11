eb741e7ebe9e2fc2b094a713a1ac41a7c0e5334a
marco <m.gablowsky@googlemail.com>
2014-10-11 18:07:34 +0200

    added shooting ability to player

--- GeoStrike/src/entities/Player.js
+++ GeoStrike/src/entities/Player.js
@@ -3,7 +3,33 @@ MAG.GeoStrike.entities.Player = function (game) {
     var _sprite = game.add.sprite(game.width / 2, game.height - 150, 'ship'),
         _speed = 7,
         _cursors = game.input.keyboard.createCursorKeys(),
+        _fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR),
+        _fireDelay = 300,
+        _beamTimer = game.time.now,
+        _beams,
         _gameConf = MAG.GeoStrike.gameConfig,
+
+        _initBeams = function () {
+            _beams = game.add.group();
+            _beams.enableBody = true;
+            _beams.physicsBodyType = _gameConf.physics.constant;
+            _beams.createMultiple('12', 'beam');
+            _beams.setAll('anchor.x', 0.5);
+            _beams.setAll('anchor.y', 1);
+            _beams.setAll('outOfBoundsKill', true);
+            _beams.setAll('checkWorldBounds', true);
+        },
+
+        _fire = function () {
+            if (game.time.now > _beamTimer) {
+                var bullet = _beams.getFirstExists(false);
+                if (bullet) {
+                    bullet.reset(_sprite.x + _sprite.width / 2, _sprite.y + 8);
+                    bullet.body.velocity.y = -400;
+                    _beamTimer = game.time.now + _fireDelay;
+                }
+            }
+        },
 
         update = function () {
             if (_cursors.up.isDown) {
@@ -19,10 +45,17 @@ MAG.GeoStrike.entities.Player = function (game) {
             else if (_cursors.right.isDown) {
                 _sprite.x += _speed;
             }
+
+            if (_fireButton.isDown) {
+                _fire();
+            }
         };
 
     game.physics.enable(_sprite, _gameConf.physics.constant);
     _sprite.body.collideWorldBounds = true;
+
+    _initBeams();
+
 
     // create debugging object for player entity
     if (MAG.GeoStrike.debug) {
@@ -38,6 +71,12 @@ MAG.GeoStrike.entities.Player = function (game) {
                     _sprite.scale.setTo(newScale, newScale);
                 }
                 return _sprite.scale;
+            },
+            fireDelay: function (newDelay) {
+                if (newDelay) {
+                    _fireDelay = newDelay;
+                }
+                return _fireDelay;
             }
         };
     }
@@ -50,4 +89,5 @@ MAG.GeoStrike.entities.Player = function (game) {
 MAG.GeoStrike.entities.Player.preload = function (game) {
     'use strict';
     game.load.image('ship', 'assets/ship.png');
+    game.load.image('beam', 'assets/beam.png');
 };
